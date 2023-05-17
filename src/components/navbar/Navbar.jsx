5a405b6df7d5229bd2a55ef1b9636f2f19060319
scaubu <s.caubu@hotmail.com>
2023-05-17 12:10:30 -0300

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {RiMenu3Line, RiCloseLin} from "react-icons/ri";
+import "./navbar.css";
+
+// Block Element Modifier
+const Navbar = () => {
+    return (
+        <div className="gpt3__navbar">
+            <div className="gpt3__navbar-links">
+
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
