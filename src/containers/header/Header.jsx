a258fab95b244fd29e46c6425c24e5c11a9dcdb7
scaubu <s.caubu@hotmail.com>
2023-05-19 18:31:00 -0300

    finish whatgpt container

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -7,28 +7,28 @@ const Header = () => {
   return (
     <div className="gpt3__header section__padding" id="home">
       <div className="gpt3__header-content">
-        <h1 className="gradient__text">
+            <h1 className="gradient__text">
           Let is Build Somethinh amazing with GPT-3 OpenAI
-        </h1>
-        <p>
+            </h1>
+         <p>
           Yet bed any for travelling assistance indulgence unpleasing. Not
           thoughts all exercise blessing. Indulgence way everything joy
           alteration boisterous the arrachment. Party we years to order allow
           asked of
-        </p>
-        <div className="gpt3__header-content__input">
-          <input type="email" placeholder="Your Email Adress" />
-          <button Type="button">Get Started</button>
-        </div>
+            </p>
+         <div className="gpt3__header-content__input">
+                <input type="email" placeholder="Your Email Adress" />
+                <button Type="button">Get Started</button>
+         </div>
 
-        <div className="gpt3__header-content__people">
-          <img src={people} alt="people" />
-          <p>1.600 people requested access a visit in last 24 hours</p>
+         <div className="gpt3__header-content__people">
+                <img src={people} alt="people" />
+                <p>1.600 people requested access a visit in last 24 hours</p>
+         </div>
         </div>
-     </div>
         <div className="gpt3__header-image">
-        <img src={ai} alt="ai" />
-      </div>
+            <img src={ai} alt="ai" />
+        </div>
     </div>
   );
 };
