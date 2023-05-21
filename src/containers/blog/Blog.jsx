1175319123fbb1591d055e8a4c77969f496e999a
scaubu <s.caubu@hotmail.com>
2023-05-21 20:36:25 -0300

    finish blog container and article component, starting with your css

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,10 +1,25 @@
 import React from "react";
+import {Article} from "../../components";
+import {blog01, blog02, blog03, blog04, blog05} from "./imports";
 import "./blog.css";
 
 const Blog = () => {
     return (
-        <div>
-            Blog
+        <div className="gpt3__blog section__padding" id="blog">
+            <div className="gpt3__blog-heading">
+                <h1 className="gradient__text">A lot is happening. We are blogging about it.</h1>
+            </div>
+            <div className="gpt3__blog-container">
+                <div className='gpt3__blog-container__groupA'>
+                    <Article imgUrl={blog01} date="Sep 26, 2021"/>
+                </div>
+                <div className='gpt3__blog-container__groupB'>
+                    <Article imgUrl={blog02} date="Sep 26, 2021"/>
+                    <Article imgUrl={blog03} date="Sep 26, 2021"/>
+                    <Article imgUrl={blog04} date="Sep 26, 2021"/>
+                    <Article imgUrl={blog05} date="Sep 26, 2021"/>
+                </div>
+            </div>
         </div>
     )
 }
